166fee84bee749e420dada083500efef7fe247f9
titan <titanicking24@gmail.com>
2024-09-22 06:15:45 +0530

    FOOTER FIXED

--- src/components/layouts/home-layout/Footer.tsx
+++ src/components/layouts/home-layout/Footer.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/Buttons';
 import Image from 'next/image';
 import React from 'react';
 
@@ -5,47 +6,62 @@ const Footer = () => {
   return (
     <div className='mt-[2rem] bg-gray-900 text-white'>
       <div className='container'>
-        <div className='py-[2rem] flex gap-[4rem]'>
-          <div className='h-[2rem] w-max'>
+        <div className='py-[2rem] flex sm:flex-row flex-col max-sm:items-center gap-[4rem] max-sm:text-center'>
+          <div className='flex flex-col gap-[2rem]'>
             <Image
               src={'/long-logo-footer.svg'}
               alt={'logo'}
               width={100}
               height={100}
-              sizes='100vh'
-              className='max-h-full'
+              className='max-w-[15rem] h-auto w-auto overflow-hidden'
               blurDataURL='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMAAAADA...'
               placeholder='blur'
               loading='lazy'
             />
-          </div>
-          <div className='max-w-[22rem]'>
-            <div className='font-bold text-[0.75rem] text-white/80'>
-              Our Goal
+            <div className='mt-[2rem] max-sm:hidden lg:hidden'>
+              If you have any concerns reguarding our service or your privacy
+              mail us on:{' '}
+              <a
+                href='mailto:abc@gmail.com'
+                className='inline text-blue-300 hover:underline'
+              >
+                mailto:abc@gmail.com
+              </a>
             </div>
-            <p className='mt-[2rem]'>
-              Feeling lonely? Our app connects you with someone new, someone you
-              don’t know—so you can share your thoughts without hesitation.
-              Whether it&apos;s things you wouldn&apos;t tell friends or family,
-              we&apos;re here to give you a safe space to open up and feel
-              heard. Join us, and find a listening ear whenever you need one
-            </p>
           </div>
-          <div className='max-w-[22rem]'>
+          <div className='max-w-[30rem] w-full flex flex-col items-stretch gap-[2rem]'>
+            <p>
+              Connect anonymously and share your thoughts in a safe space. Find
+              a listening ear whenever you need it.
+            </p>
+            <Button>Join the Waitlist</Button>
+          </div>
+
+          <div className='max-w-[22rem] sm:max-lg:hidden'>
             <div className='font-bold text-[0.75rem] text-white/80'>
               Need Help? contact us
             </div>
-            <div className='flex flex-col mt-[2rem] gap-[1rem]'>
-              <p>Email</p>
-              <p>Phone</p>
+            <div className='mt-[2rem] '>
+              If you have any concerns reguarding our service or your privacy
+              mail us on:{' '}
+              <a
+                href='mailto:abc@gmail.com'
+                className='inline text-blue-300 hover:underline'
+              >
+                mailto:abc@gmail.com
+              </a>
+            </div>
+            <div className='flex gap-[1rem] sm:hidden justify-center mt-[2rem]'>
+              <SocialMediaButton imgUrl='/instagram.png' />
+              <SocialMediaButton imgUrl='/facebook-f.png' />
             </div>
           </div>
         </div>
         <div className='py-[2rem] border-t-[1px] border-t-gray-500 flex justify-between items-end'>
-          <p>
+          <p className='max-sm:text-center'>
             &copy; copyright - 2024 . created and maintained by Tamojit and Team
           </p>
-          <div className='flex gap-[1rem]'>
+          <div className='flex gap-[1rem] max-sm:hidden'>
             <SocialMediaButton imgUrl='/instagram.png' />
             <SocialMediaButton imgUrl='/facebook-f.png' />
           </div>
